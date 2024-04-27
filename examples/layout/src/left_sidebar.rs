11d7f16134eb28b62681e41000012cbbb8d14ece
Nico Just <mail@njust.de>
2024-04-27 09:51:08 +0200

    Use v_stack instead of virtual_stack

--- examples/layout/src/left_sidebar.rs
+++ examples/layout/src/left_sidebar.rs
@@ -4,8 +4,7 @@ use floem::{
     reactive::create_signal,
     view::View,
     views::{
-        container, h_stack, label, scroll, v_stack, virtual_stack, Decorators, VirtualDirection,
-        VirtualItemSize,
+        container, h_stack, label, scroll, v_stack, v_stack_from_iter, Decorators,
     },
 };
 
@@ -19,27 +18,19 @@ pub fn left_sidebar_view() -> impl View {
     let top_bar = label(|| String::from("Top bar"))
         .style(|s| s.padding(10.0).width_full().height(TOPBAR_HEIGHT));
 
-    let side_bar = scroll({
-        virtual_stack(
-            VirtualDirection::Vertical,
-            VirtualItemSize::Fixed(Box::new(|| 22.0)),
-            move || long_list.get(),
-            move |item| *item,
-            move |item| {
-                label(move || item.to_string()).style(move |s| {
-                    s.padding(10.0)
-                        .padding_top(3.0)
-                        .padding_bottom(3.0)
-                        .width(SIDEBAR_WIDTH)
-                        .items_start()
-                        .border_bottom(1.0)
-                        .border_color(Color::rgb8(205, 205, 205))
-                })
-            },
-        )
-        .style(|s| s.flex_col().width(SIDEBAR_WIDTH - 1.0))
-    })
-    .style(|s| {
+    let list = v_stack_from_iter(long_list.get().into_iter().map(|item| {
+        label(move || item.to_string()).style(move |s| {
+            s.padding(10.0)
+                .padding_top(3.0)
+                .padding_bottom(3.0)
+                .width(SIDEBAR_WIDTH)
+                .items_start()
+                .border_bottom(1.0)
+                .border_color(Color::rgb8(205, 205, 205))
+        })
+    }));
+
+    let side_bar = scroll( list.style(|s| s.flex_col().width(SIDEBAR_WIDTH - 1.0))).style(|s| {
         s.width(SIDEBAR_WIDTH)
             .height_full()
             .border_right(1.0)
@@ -61,7 +52,6 @@ pub fn left_sidebar_view() -> impl View {
     });
 
     let content = h_stack((side_bar, main_window)).style(|s| s.height_full().width_full());
-
     let view = v_stack((top_bar, content)).style(|s| s.width_full().height_full());
 
     let id = view.id();
